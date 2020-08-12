fd1411bf07241eb0e3b68a1eec83b88864babb9f
Kitty-Cats <joshua@Earthbook.local>
2020-08-12 18:36:22 +1200

    add command line argument for size

--- triangle.c
+++ triangle.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <libc.h>
 
 int size=7;
 char* letter="X";
 int main(int argv, char** argc){
     letter=argc[1];
+    size=atoi(argc[2]);
     int row;
     int width;
     for(row=0;row<size;row++){
